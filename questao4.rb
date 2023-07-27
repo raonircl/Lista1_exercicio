eefbac138e5e43105344e0a60920351e50b2f3e0
raonircl <raoni.afi@gmail.com>
2023-07-27 20:50:24 -0300

    ler numero inteiro e mostrar o dobro

--- questao4.rb
+++ questao4.rb
@@ -1,8 +1,6 @@
 #Crie um script em Ruby que leia um número inteiro e mostre o dobro desse número.
 
-num1 = gets.chomp.to_i 
-num2 = gets.chomp.to_i
-
-mult = (num1 + num2) * 2
+num = gets.chomp.to_i 
+mult = num * 2
 
 puts mult

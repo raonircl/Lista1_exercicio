2e22df8aa7e0fd2d8e6a27b70223e7352a5147ab
raonircl <raoni.afi@gmail.com>
2023-07-27 21:24:26 -0300

    dias de vida

---
+++ questao8.rb
@@ -0,0 +1,6 @@
+#Crie um script em Ruby que leia uma idade e calcule quantos dias essa pessoa já viveu. Considere que 1 ano tem 365 dias.
+
+pessoa_idade = gets.chomp.to_i
+dias_vida = pessoa_idade * 365
+
+puts dias_vida

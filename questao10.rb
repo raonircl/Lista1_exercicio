b0043fe05beaa9de7b67e2ff179645f53e3eb4c1
raonircl <raoni.afi@gmail.com>
2023-07-27 22:43:33 -0300

    calculo, mostrar soma, produro e quociente

---
+++ questao10.rb
@@ -0,0 +1,12 @@
+#Crie um script em Ruby que leia dois números reais, calcule e mostre a soma deles, o produto e o quociente.
+
+y = gets.chomp.to_f
+x = gets.chomp.to_f
+
+soma = x + y
+produto = x * y
+quociente = x / y
+
+puts soma
+puts produto
+puts quociente
